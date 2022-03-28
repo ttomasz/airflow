a422b641807e1ab5437926a73bc3c1718dc314c1
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-03-28 09:31:00 -0500

    Fix argument count errors

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2036,10 +2036,9 @@ class Airflow(AirflowBaseView):
             start_date,
             end_date,
             origin,
-            request,
-            recursive,
-            confirmed,
-            only_failed,
+            recursive=recursive,
+            confirmed=confirmed,
+            only_failed=only_failed,
         )
 
     @expose('/dagrun_clear', methods=['POST'])
@@ -2062,7 +2061,12 @@ class Airflow(AirflowBaseView):
         end_date = dr.logical_date
 
         return self._clear_dag_tis(
-            dag, start_date, end_date, recursive=True, confirmed=confirmed, request=request, origin=None
+            dag,
+            start_date,
+            end_date,
+            origin=None,
+            recursive=True,
+            confirmed=confirmed,
         )
 
     @expose('/blocked', methods=['POST'])
